c3b3480d5c2c5d02fa0367c5b1377f3757742230
mioky98 <mo981014@gmail.com>
2021-11-17 16:17:20 +0900

    [feat]새로운 할인 정책 개발

---
+++ study03/core/src/main/java/hello/core/CoreApplication.java
@@ -0,0 +1,30 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		OrderService orderService = new OrderServiceImpl();
+
+		long memberId = 1L;
+
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+		System.out.println("order = " + order);
+
+	}
+
+}
