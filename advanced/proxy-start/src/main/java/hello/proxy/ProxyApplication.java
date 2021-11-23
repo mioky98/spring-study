e4d034e4f0d9edb23fca40213d7f0cdd797dabb3
mioky98 <mo981014@gmail.com>
2021-11-23 18:24:45 +0900

    [feat] Decorator pattern + log

--- advanced/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ advanced/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,10 +2,15 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
@@ -13,4 +18,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
